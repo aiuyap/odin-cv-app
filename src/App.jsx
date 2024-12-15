12b3273961bd5383066ae35431eea5a3d332d372
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-15 08:36:26 +0800

    feat: created rough layout for each field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { PersonalInfo } from "./components/PersonalInfo";
-
 export function App() {
   return (
-    <>
+    <div className="container">
       <h1>CV Builder</h1>
-      <div>
-        <PersonalInfo></PersonalInfo>
-      </div>
-    </>
+      <fieldset>
+        <legend>General Information</legend>
+        <form>
+          <label>
+            Name: <input type="text" />
+          </label>
+          <label>
+            Address: <input type="text" />
+          </label>
+          <label>
+            Age: <input type="text" />
+          </label>
+          <label>
+            Email: <input type="email" />
+          </label>
+          <label>
+            Phone Number: <input type="number" max={3} />
+          </label>
+        </form>
+      </fieldset>
+    </div>
   );
 }
